6c931e92e321c91a1bb64c74ab919e1dee69fd51
y.el-haddady <youssef.el-haddady@nimbelways.com>
2022-07-03 12:00:43 +0100

    implementation with flagsmith

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import flagsmith from "flagsmith";
+import "./App.css";
 
 function App() {
+  const [stories, setStories] = useState([]);
+  const [message, setMessage] = useState("loading...");
+  const [showStoryPoints, setShowStoryPoints] = useState(false);
+  useEffect(() => {
+    async function fetchNewsStories() {
+      try {
+        const data = await (await fetch("https://hn.algolia.com/api/v1/search_by_date?tags=front_page")).json();
+        setStories(data.hits);
+        const message = data.hits.length ? "" : "No stories found";
+        setMessage(message);
+      } catch (err) {
+        console.log(`err: ${err.mesasge}`, err);
+        setMessage("could not fetch stories");
+      }
+    }
+    fetchNewsStories();
+    const flagsKey = process.env.REACT_APP_FLAGS_KEY;
+    flagsmith.init({
+      environmentID: flagsKey,
+      cacheFlags: true,
+      enableAnalytics: true,
+      onChange: (oldFlags, params) => {
+        setShowStoryPoints(flagsmith.hasFeature("show_story_points"));
+      },
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Latest HN Stories</h2>
+        {message}
+        <div className="stories">
+          {Array.isArray(stories) &&
+            stories.map(
+              (story) =>
+                story.url && (
+                  <h3>
+                    <a href={story.url} target="_blank" rel="noreferrer">
+                      {story.title}
+                    </a>{" "}
+                    - by {story.author} {showStoryPoints ? "- points " + story.points : ""}
+                  </h3>
+                )
+            )}
+        </div>
       </header>
     </div>
   );
