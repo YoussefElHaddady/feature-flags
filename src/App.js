d81fdbc4dc8bc3fa1e6af3b1d2795c2b1a732b84
y.el-haddady <youssef.el-haddady@nimbelways.com>
2022-07-03 13:47:59 +0100

    implementation with react-feature-flags library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Flags } from "react-feature-flags";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Feature flags with react-feature-flags</h1>
+        </header>
+        <Flags
+          authorizedFlags={["vipOnly"]}
+          renderOn={(flag) => {
+            console.log(flag);
+            return <h1>VIP (flag: {flag.map((f) => f.name).join(", ")})</h1>;
+          }}
+          renderOff={() => <h1>NO VIP</h1>}
+        />
+        <Flags authorizedFlags={["vipOnly"]}>
+          <h1>VIP (children props)</h1>
+        </Flags>
+        <Flags authorizedFlags={["adminOnly", "vipOnly"]} exactFlags renderOn={(flag) => <h1>Admin and VIP (flag: {flag.map((f) => f.name).join(", ")})</h1>} />
+        <footer>End</footer>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/flags.js
@@ -0,0 +1,6 @@
+const flags = [
+  { name: "vipOnly", isActive: true },
+  { name: "adminOnly", isActive: false },
+];
+
+export default flags;
